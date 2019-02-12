1c4954da2a927e5c922076d39fc0b740129d590c
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-12 14:18:50 -0500

    created page with props

--- Card.js
+++ Card.js
@@ -1,19 +1,20 @@
 // EVERY REACT COMPONENT MUST RETURN A SINGLE DOM ELEMENT
 function Card(props){
     console.log(props);
-    const name = "Sean McQuaid";
+    const course = props.data.course;
     // const title = "React From the Beginning";
-    const title = props.title;
+    const instructor = props.data.instructor;
+    const image = props.data.image;
     const saleOn = false;
     return (
         <div className="col s3">
             <div className="card hoverable small">
                 <div className="card-image">
-                    <img src="http://lorempixel.com/400/400/nature/" />
+                    <img src={image} />
                 </div>
                 <div className="card-content">
-                    <p>{title}</p>
-                    <p>{name}</p>
+                    <p>{course}</p>
+                    <p>{instructor}</p>
                 </div>
                 <div className="card-action">
                     <a href="#">${saleOn ? 9.99 : 59.99}</a>
